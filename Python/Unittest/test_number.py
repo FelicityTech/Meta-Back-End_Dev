5838346efc1d2ccba628677bb41675ccae94a716
Solomon Eniola Adegoke <souceking@gmail.com>
2023-03-02 21:22:36 +0100

    Unittest tutorial codes

---
+++ Python/Unittest/test_number.py
@@ -0,0 +1,11 @@
+# import unittest
+
+# class NumbersTest(unittest.TestCase):
+#     def test_even(self):
+#         """
+#         Test that numbers between 0 and 5 are all even.
+#         """
+
+#         for i in range(0, 6):
+#             with.self.SubTest(i=i):
+#                 self.asserttEqual(1)
